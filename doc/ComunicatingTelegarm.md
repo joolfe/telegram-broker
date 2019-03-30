7fa73c212c98085058738046d11619ee7019ba2f
joliva <joolfe04@gmail.com>
2019-03-30 20:40:17 +0100

    Starting with telegram lib

---
+++ doc/ComunicatingTelegarm.md
@@ -0,0 +1,21 @@
+# Comunicating with Telegram
+
+Let's start testing the library, just to remember we are going to give a try to this one [telegram-mtproto](https://www.npmjs.com/package/telegram-mtproto), so first step is to install the dependency using npm like this:
+
+```
+npm install --save telegram-mtproto@2.2.8
+```
+
+Now we can start using the library, at first stage we will start soing all our code and test in the `index.js` file, we will refactor or create modules in the future when the application start growing.
+
+## Initializing the client
+
+// Aqui poner lo de la config
+
+## Authorization flow
+
+// poner el link a telegarm
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,47 @@
 
-console.log("Hello telegram!");
+const { MTProto } = require('telegram-mtproto');
+
+console.log("We are running!");
+
+const phone = {
+  num : '+34636640130',
+  code: '22222'
+}
+
+const api = {
+  layer          : 57,
+  initConnection : 0x69796de9,
+  api_id         : 805232
+}
+
+const app_hash = 'eafd0e465f73c807b0a97793638b2d97';
+
+const server = {
+  dev: true //We will connect to the test server.
+}
+
+const client = MTProto({ server, api });
+
+async function connect(){
+  
+  const { phone_code_hash } = await client('auth.sendCode', {
+    phone_number  : phone.num,
+    current_number: false,
+    api_id        : api.api_id,
+    api_hash      : app_hash
+  })
+
+  console.log('WOW we get domething? ', phone_code_hash)
+
+/*
+  const { user } = await client('auth.signIn', {
+    phone_number   : phone.num,
+    phone_code_hash: phone_code_hash,
+    phone_code     : phone.code
+  })
+*/
+  console.log('WOW signed as ')
+
+}
+
+connect()
